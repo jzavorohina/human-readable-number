34010e0e114daee4214547f4f2c737f601ab1742
Julia Zavorokhina <jzavorohina@gmail.com>
2020-03-22 23:19:41 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+
+    const DIGITS = {
+        0: '',
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+    };
+
+    const TEENS = {
+        0: 'ten',
+        1: 'eleven',
+        2: 'twelve',
+        3: 'thirteen',
+        4: 'fourteen',
+        5: 'fifteen',
+        6: 'sixteen',
+        7: 'seventeen',
+        8: 'eighteen',
+        9: 'nineteen',
+    };
+
+    const TENS = {
+        0: ' ',
+        2: 'twenty',
+        3: 'thirty',
+        4: 'forty',
+        5: 'fifty',
+        6: 'sixty',
+        7: 'seventy',
+        8: 'eighty',
+        9: 'ninety',
+    };
+
+    let [digit, tens, hundreds] = number.toString().split('').reverse();
+
+    return ((hundreds ? `${DIGITS[hundreds]} hundred ` : '')
+        + (tens ? `${TENS[tens] || TEENS[digit]} ` : '')
+        + ((tens !== '1' && digit) ? DIGITS[digit] : ''))
+        .replace('  ', ' ')
+        .replace('  ', ' ')
+        .trim() || 'zero';
+
 }
